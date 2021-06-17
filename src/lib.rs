02945a89aa6b4fbe8c23de5a333d9e636f884a95
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2021-06-17 21:40:19 +0200

    VsockThread: Handle requests on the host uds_path
    Create a UnixListener to listen to requests on the host uds_path
    i.e from the application running on the host. The UnixListener is
    registered with the VringWorker's epoll to listen for custom events
    which then triggers backend's handle_event when an event is
    triggered.
    
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,18 @@ extern crate vhost;
 extern crate vhost_user_backend;
 extern crate virtio_bindings;
 extern crate vm_memory;
+extern crate vmm_sys_util;
 
 use core::slice;
+use log::*;
 use std::io;
 use std::mem;
+use std::os::unix::net::UnixListener;
+use std::os::unix::prelude::AsRawFd;
+use std::os::unix::prelude::RawFd;
 use std::process;
 use std::result;
+use std::sync::Mutex;
 use std::sync::{Arc, RwLock};
 use vhost::vhost_user::message::VhostUserProtocolFeatures;
 use vhost::vhost_user::message::VhostUserVirtioFeatures;
@@ -18,10 +24,16 @@ use vhost::vhost_user::Listener;
 use vhost_user_backend::VhostUserBackend;
 use vhost_user_backend::VhostUserDaemon;
 use vhost_user_backend::Vring;
+use vhost_user_backend::VringWorker;
 use virtio_bindings::bindings::virtio_blk::__u64;
 use virtio_bindings::bindings::virtio_net::VIRTIO_F_VERSION_1;
 use vm_memory::GuestMemoryAtomic;
 use vm_memory::GuestMemoryMmap;
+use vmm_sys_util::eventfd::EventFd;
+use vmm_sys_util::eventfd::EFD_NONBLOCK;
+
+// Notification coming from the backend.
+const BACKEND_EVENT: u16 = 3;
 
 #[derive(Debug)]
 /// This structure is the public API through which an external program
@@ -61,16 +73,61 @@ impl VsockConfig {
     }
 }
 
-struct VhostUserVsockBackend {
+struct VhostUserVsockThread {
+    mem: Option<GuestMemoryAtomic<GuestMemoryMmap>>,
     event_idx: bool,
+    host_sock: RawFd,
+    kill_evt: EventFd,
+    vring_worker: Option<Arc<VringWorker>>,
+}
+
+impl VhostUserVsockThread {
+    fn new(uds_path: String) -> Self {
+        // TODO: better error handling
+        let host_sock = UnixListener::bind(&uds_path)
+            .and_then(|sock| sock.set_nonblocking(true).map(|_| sock))
+            .unwrap();
+
+        VhostUserVsockThread {
+            mem: None,
+            event_idx: false,
+            host_sock: host_sock.as_raw_fd(),
+            kill_evt: EventFd::new(EFD_NONBLOCK).unwrap(),
+            vring_worker: None,
+        }
+    }
+
+    fn set_vring_worker(&mut self, vring_worker: Option<Arc<VringWorker>>) {
+        self.vring_worker = vring_worker;
+        self.vring_worker
+            .as_ref()
+            .unwrap()
+            .register_listener(
+                self.host_sock,
+                epoll::Events::EPOLLIN,
+                u64::from(BACKEND_EVENT),
+            )
+            .unwrap();
+    }
+}
+
+struct VhostUserVsockBackend {
     guest_cid: __u64,
+    threads: Vec<Mutex<VhostUserVsockThread>>,
+    queues_per_thread: Vec<u64>,
 }
 
 impl VhostUserVsockBackend {
-    fn new(guest_cid: u64) -> Self {
+    fn new(guest_cid: u64, uds_path: String) -> Self {
+        let mut threads = Vec::new();
+        let thread = Mutex::new(VhostUserVsockThread::new(uds_path));
+        let mut queues_per_thread = Vec::new();
+        queues_per_thread.push(0b11);
+        threads.push(thread);
         VhostUserVsockBackend {
-            event_idx: false,
             guest_cid: guest_cid,
+            threads: threads,
+            queues_per_thread: queues_per_thread,
         }
     }
 }
@@ -93,13 +150,18 @@ impl VhostUserBackend for VhostUserVsockBackend {
     }
 
     fn set_event_idx(&mut self, enabled: bool) {
-        self.event_idx = enabled;
+        for thread in self.threads.iter() {
+            thread.lock().unwrap().event_idx = enabled;
+        }
     }
 
     fn update_memory(
         &mut self,
-        _atomic_mem: GuestMemoryAtomic<GuestMemoryMmap>,
+        atomic_mem: GuestMemoryAtomic<GuestMemoryMmap>,
     ) -> result::Result<(), io::Error> {
+        for thread in self.threads.iter() {
+            thread.lock().unwrap().mem = Some(atomic_mem.clone());
+        }
         Ok(())
     }
 
@@ -122,6 +184,10 @@ impl VhostUserBackend for VhostUserVsockBackend {
         };
         buf.to_vec()
     }
+
+    fn queues_per_thread(&self) -> Vec<u64> {
+        self.queues_per_thread.clone()
+    }
 }
 
 /// This is the public API through which an external program starts the
@@ -129,12 +195,26 @@ impl VhostUserBackend for VhostUserVsockBackend {
 pub fn start_backend_server(vsock_config: VsockConfig) {
     let vsock_backend = Arc::new(RwLock::new(VhostUserVsockBackend::new(
         vsock_config.guest_cid,
+        vsock_config.uds_path,
     )));
 
     let listener = Listener::new(vsock_config.socket, true).unwrap();
 
     let name = "vhost-user-vsock";
     let mut vsock_daemon = VhostUserDaemon::new(name.to_string(), vsock_backend.clone()).unwrap();
+
+    let mut vring_workers = vsock_daemon.get_vring_workers();
+
+    if vring_workers.len() != vsock_backend.read().unwrap().threads.len() {
+        error!("Number of vring workers must be identical to number of backend threads");
+    }
+
+    for thread in vsock_backend.read().unwrap().threads.iter() {
+        thread
+            .lock()
+            .unwrap()
+            .set_vring_worker(Some(vring_workers.remove(0)));
+    }
     if let Err(e) = vsock_daemon.start(listener) {
         dbg!("Failed to start vsock daemon: {:?}", e);
         process::exit(1);
@@ -143,6 +223,14 @@ pub fn start_backend_server(vsock_config: VsockConfig) {
     if let Err(e) = vsock_daemon.wait() {
         dbg!("Error from daemon main thread: {:?}", e);
         process::exit(1);
+    }
+
+    debug!("Vsock daemon is finished");
+
+    for thread in vsock_backend.read().unwrap().threads.iter() {
+        if let Err(e) = thread.lock().unwrap().kill_evt.write(1) {
+            error!("Error shutting down worker thread: {:?}", e);
+        }
     }
 }
 
