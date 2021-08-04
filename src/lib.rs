af8edeb8a4f5085728d2f272fc8b74f723649577
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2021-08-04 15:03:14 +0200

    refactor: move txbuf into separate file
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -59,6 +59,9 @@ use vm_memory::GuestMemoryMmap;
 use vmm_sys_util::eventfd::EventFd;
 use vmm_sys_util::eventfd::EFD_NONBLOCK;
 
+mod txbuf;
+use txbuf::*;
+
 const NUM_QUEUES: usize = 2;
 const QUEUE_SIZE: usize = 256;
 
@@ -140,8 +143,9 @@ const VSOCK_FLAGS_SHUTDOWN_SEND: u32 = 2;
 
 type Result<T> = std::result::Result<T, Error>;
 
+/// Below enum defines custom error types
 #[derive(Debug)]
-enum Error {
+pub enum Error {
     /// Failed to handle event other than EPOLLIN event
     HandleEventNotEpollIn,
     /// Failed to handle unknown event
@@ -1604,117 +1608,6 @@ impl VsockConnection {
     /// more data to it.
     fn need_credit_update_from_peer(&self) -> bool {
         self.peer_avail_credit() == 0
-    }
-}
-
-#[derive(Debug, Clone)]
-struct LocalTxBuf {
-    /// Buffer holding data to be forwarded to a host-side application
-    buf: Vec<u8>,
-    /// Index into buffer from which data can be consumed from the buffer
-    head: Wrapping<u32>,
-    /// Index into buffer from which data can be added to the buffer
-    tail: Wrapping<u32>,
-}
-
-impl LocalTxBuf {
-    /// Create a new instance of LocalTxBuf
-    fn new() -> Self {
-        Self {
-            buf: vec![0; CONN_TX_BUF_SIZE as usize],
-            head: Wrapping(0),
-            tail: Wrapping(0),
-        }
-    }
-
-    /// Check if the buf is empty
-    fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
-
-    /// Add new data to the tx buffer, push all or none
-    /// Returns LocalTxBufFull error if space not sufficient
-    fn push(&mut self, data_buf: &[u8]) -> Result<()> {
-        if CONN_TX_BUF_SIZE as usize - self.len() < data_buf.len() {
-            return Err(Error::LocalTxBufFull);
-        }
-
-        // if self.tail.0 > self.head.0 {
-        //     // Data to be pushed at the end of the buffer
-        //     if self.tail.0 as usize + buf_len <= CONN_TX_BUF_SIZE as usize {
-        //         // Entire buffer fits at the end of the local tx buffer
-        //         self.buf[start as usize..(start as usize + buf_len as usize)].clone_from_slice(buf);
-        //         self.tail.0 = (self.tail.0 + buf_len as u32) % CONN_TX_BUF_SIZE;
-        //     } else {
-        //         // Only part of the buffer fits at the end of the local tx buffer
-        //         self.buf[start as usize..]
-        //             .clone_from_slice(&buf[..(CONN_TX_BUF_SIZE - start) as usize]);
-        //         self.buf[0..].clone_from_slice(&buf[(CONN_TX_BUF_SIZE - start) as usize..]);
-        //         self.tail.0 = CONN_TX_BUF_SIZE - start;
-        //     }
-        // } else if self.tail.0 < self.head.0 {
-        //     // Data should fit here as checks are performed above
-        //     self.buf[start as usize..(start as usize + buf_len as usize)].clone_from_slice(buf);
-        //     self.tail.0 = (self.tail.0 + buf_len as u32) % CONN_TX_BUF_SIZE;
-        // }
-        let tail_idx = self.tail.0 as usize % CONN_TX_BUF_SIZE as usize;
-
-        // Check if we can fit the data buffer between head and end of buffer
-        let len = std::cmp::min(CONN_TX_BUF_SIZE as usize - tail_idx, data_buf.len());
-        self.buf[tail_idx..tail_idx + len].copy_from_slice(&data_buf[..len]);
-
-        // Check if there is more data to be wrapped around
-        if len < data_buf.len() {
-            self.buf[..(data_buf.len() - len)].copy_from_slice(&data_buf[len..]);
-        }
-
-        self.tail += Wrapping(data_buf.len() as u32);
-
-        Ok(())
-    }
-
-    /// Flush buf data to stream
-    fn flush_to(&mut self, stream: &mut UnixStream) -> Result<usize> {
-        // TODO: Check if CREDIT_UPDATE packet needs to be sent after this call
-        if self.is_empty() {
-            return Ok(0);
-        }
-
-        // if self.tail.0 > self.head.0 {
-        //     // Data to be flushed lies between head and tail
-        //     let written = stream
-        //         .write(&self.buf[self.head.0 as usize..self.tail.0 as usize])
-        //         .map_err(Error::LocalTxBufFlush)?;
-        //     self.head += Wrapping(written as u32);
-        //     return Ok(written);
-        // } else {
-        //     // Data to be flushed lies between head and end of buffer
-        //     // and between start of buffer to tail
-        //     let written = stream
-        //         .write(&self.buf[self.head.0 as usize..])
-        //         .map_err(Error::LocalTxBufFlush)?;
-        //     self.head.0 = (self.head + Wrapping(written as u32)).0 % CONN_TX_BUF_SIZE;
-        //     return Ok(written);
-        // }
-        let head_idx = self.head.0 as usize % CONN_TX_BUF_SIZE as usize;
-
-        // First write from head to end of buffer
-        let len = std::cmp::min(CONN_TX_BUF_SIZE as usize - head_idx, self.len());
-        let written = stream
-            .write(&self.buf[head_idx..(head_idx + len)])
-            .map_err(Error::LocalTxBufFlush)?;
-
-        self.head += Wrapping(written as u32);
-        if written < len {
-            return Ok(written);
-        }
-
-        Ok(written + self.flush_to(stream).unwrap_or(0))
-    }
-
-    /// Return amount of data in the buffer
-    fn len(&self) -> usize {
-        (self.tail - self.head).0 as usize
     }
 }
 
